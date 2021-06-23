5a1f8813200a1bfddcf9969dd4b0daac2b7a31d7
Jason <tsai301103@gmail.com>
2021-06-23 22:18:41 +0800

    new slider func with label show

--- test_git/ViewController.swift
+++ test_git/ViewController.swift
@@ -10,17 +10,28 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var number: UIButton!
+    @IBOutlet weak var aguilife: UISlider!
+    @IBOutlet weak var aguilife_num: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
     }
 
     @IBAction func press(_ sender: Any) {
-        
         print(number.currentTitle!)
         
     }
+ 
+    @IBAction func sliding(_ sender: UISlider) {
+        aguilife_num.text=String(sender.value.rounded())
+        
+      
+        
+    }
+    
     
 
 }
