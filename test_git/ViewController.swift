ac99ca1a79aca5b75273888d6591eb1dacd3f20b
Jason <tsai301103@gmail.com>
2021-06-23 20:09:37 +0800

    built a press as "5"

--- test_git/ViewController.swift
+++ test_git/ViewController.swift
@@ -8,12 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var number: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func press(_ sender: Any) {
+        
+        print(number.currentTitle!)
+        
+    }
+    
 
 }
 
